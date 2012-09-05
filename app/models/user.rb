6031ae6294c6d9b17587b41a45489c005ba720a9
Georgia A <and.georgia.m@gmail.com>
2012-09-04 20:51:06 -0700

    added password/encryption to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-  attr_accessible :description, :email, :name, :phone
+ attr_accessible :description, :email, :name, :phone, :password, :password_confirmation
+ has_secure_password
+  
+ validates :password, presence: true, length: { minimum: 6 }
+ validates :password_confirmation, presence: true
+  
 end
