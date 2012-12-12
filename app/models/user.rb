c097bcc716509afd7d3a452becf5ec244509b34c
Georgia A <and.georgia.m@gmail.com>
2012-12-11 20:55:27 -0800

    fixed presence-of-name for user, refactored link/user model specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@ class User < ActiveRecord::Base
   #validates :password, presence: true, length: { minimum: 6 }
   #validates :password_confirmation, presence: true
   
+  validates :name, presence: true
+  
   private
     def create_remember_token
       self.remember_token = SecureRandom.urlsafe_base64
