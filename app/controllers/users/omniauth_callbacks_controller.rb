a9b1f5a712286d6b4e0e66f95ba8584c5920d6e3
Georgia A <and.georgia.m@gmail.com>
2014-09-01 15:51:56 -0700

    ignore email for github logins, add image_url to model
    whoops

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,7 +6,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user
       set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
     else
-      session["devise.github_data"] = request.env["omniauth.auth"]
+      session["devise.user_attributes"] = @user.attributes
       redirect_to new_user_registration_url
     end
   end
